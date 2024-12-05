9c4efe6222ffbff04cb205e4e68621821df05d02
dvcsr <4th3sh0ws@gmail.com>
2024-12-05 14:50:54 +0700

    init repo

---
+++ src/main/java/com/authex/authex/AuthexApplication.java
@@ -0,0 +1,13 @@
+package com.authex.authex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authex/authex/AuthexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authex.authex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
